e603ecf4afac2be150a3b551cf0b355188dd2dc3
asterisk1o9 <asterisk1o9@gmail.com>
2022-06-09 15:55:29 -0400

    Initial Commit

---
+++ PalindromeNumber/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PalindromeNumber
+//
+//  Created by Marvin Augustin on 6/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
